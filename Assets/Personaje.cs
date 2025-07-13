11a55c100bd418b6db6e813511e80bce222f0a3c
aniokku <>
2025-07-13 11:42:21 -0500

    Encapsulacion y abstraccion

---
+++ Assets/Personaje.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    // Encapsulacion con campos privados y propiedades publicas
+    private string nombre;
+    private int fuerza;
+    private int defensa;
+    private int vida;
+    private int agilidad;
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public int Fuerza { get => fuerza; set => fuerza = value; }
+    public int Defensa { get => defensa; set => defensa = value; }
+    public int Vida { get => vida; set => vida = value; }
+    public int Agilidad { get => agilidad; set => agilidad = value; }
+
+    // Abstraccion: metodo de alto nivel que oculta los detalles internos
+    
+    public virtual void Atacar()
+    {
+        int daño = CalcularDaño();
+        Debug.Log($"{Nombre} ataca e inflige {daño} de daño.");
+    }
+
+    protected int CalcularDaño()
+    {
+        int bonus = agilidad > 5 ? 2 : 0;
+        return fuerza + bonus;
+    }
+}
